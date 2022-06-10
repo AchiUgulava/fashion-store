72b63255c4ce9e8675176278e6c7ed263214d608
Achi <achiugulava@gmail.com>
2022-06-10 17:01:10 +0400

    first stable build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Products/Product";
+import Footer from "./components/Footer";
+import Categories from "./pages/Categories";
+import { SnackbarProvider } from "notistack";
+import { Slide } from "@mui/material";
+import Checkout from "./pages/Checkout";
+import Register from "./pages/Register.js";
 
-function App() {
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-full mx-auto">
+        <Router >
+          <SnackbarProvider
+            TransitionComponent={Slide}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            <Navbar>
+            <Routes className="absolute max-w-6xl">
+              <Route path="/" element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="checkout" element={<Checkout />} />
+              <Route path="shop">
+                <Route index element={<Categories />}/>
+                <Route path=":categoryName">
+                  <Route index element={<Products/>}/>
+                  <Route path=':productId' element={<Product/>}/>
+                </Route>
+                <Route path='*' element={"why RE YOU HERE"}/>
+              </Route>
+            </Routes>
+            </Navbar>
+          </SnackbarProvider>
+        </Router>
+        <Footer />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,270 @@
+export const LargeMenuItems = [
+  {
+    id: 1,
+    title: "WOMENS",
+    image:
+      "https://media.istockphoto.com/photos/polka-dot-summer-brown-dress-suede-wedge-sandals-eco-straw-tote-bag-picture-id1208148708?k=20&m=1208148708&s=612x612&w=0&h=rjZiAPCOpwREiTET21lTP3wM30BUqAG9PjocC-euJ98=",
+    route: "/shop/womens",
+  },
+  {
+    id: 2,
+    title: "MENS",
+    image:
+      "https://res.cloudinary.com/trunk-club/image/upload/f_auto,q_auto/public_site/2020/Style%20Inspiration/TCM/20058_TCM_BlogHero_WearToWork_Mobile_01.jpg",
+    route: "/shop/mens",
+  },
+];
+
+export const SmallMenuItems = [
+  {
+    id: 1,
+    title: "JACKETS",
+    image:
+      "https://media.istockphoto.com/photos/man-straightens-his-jacket-isolated-on-light-background-picture-id901960792?b=1&k=20&m=901960792&s=170667a&w=0&h=d8GQLj9g94ckTKdLA-eHgV8DTuU5F7xjSTQaWv47APE=",
+    route: "/shop/jackets",
+  },
+  {
+    id: 2,
+    title: "SHOES",
+    image:
+      "https://img.freepik.com/free-photo/different-women-s-shoes-floor-top-view-copy-space_175682-11014.jpg",
+    route: "/shop/sneakers",
+  },
+  {
+    id: 3,
+    title: "HATS",
+    image:
+      "https://images.pexels.com/photos/458649/pexels-photo-458649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    route: "/shop/hats",
+  },
+];
+
+export const products = {
+  hats: [
+    {
+      id: 1,
+      name: "Brown Brim",
+      imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+      price: 25,
+    },
+    {
+      id: 2,
+      name: "Blue Beanie",
+      imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
+      price: 18,
+    },
+    {
+      id: 3,
+      name: "Brown Cowboy",
+      imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
+      price: 35,
+    },
+    {
+      id: 4,
+      name: "Grey Brim",
+      imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
+      price: 25,
+    },
+    {
+      id: 5,
+      name: "Green Beanie",
+      imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
+      price: 18,
+    },
+    {
+      id: 6,
+      name: "Palm Tree Cap",
+      imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
+      price: 14,
+    },
+    {
+      id: 7,
+      name: "Red Beanie",
+      imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
+      price: 18,
+    },
+    {
+      id: 8,
+      name: "Wolf Cap",
+      imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
+      price: 14,
+    },
+    {
+      id: 9,
+      name: "Blue Snapback",
+      imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
+      price: 16,
+    },
+  ],
+  mens: [
+    {
+      id: 30,
+      name: "Camo Down Vest",
+      imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
+      price: 325,
+    },
+    {
+      id: 31,
+      name: "Floral T-shirt",
+      imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
+      price: 20,
+    },
+    {
+      id: 32,
+      name: "Black & White Longsleeve",
+      imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
+      price: 25,
+    },
+    {
+      id: 33,
+      name: "Pink T-shirt",
+      imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
+      price: 25,
+    },
+    {
+      id: 34,
+      name: "Jean Long Sleeve",
+      imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
+      price: 40,
+    },
+    {
+      id: 35,
+      name: "Burgundy T-shirt",
+      imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
+      price: 25,
+    },
+  ],
+  womens: [
+    {
+      id: 23,
+      name: "Blue Tanktop",
+      imageUrl: "https://i.ibb.co/7CQVJNm/blue-tank.png",
+      price: 25,
+    },
+    {
+      id: 24,
+      name: "Floral Blouse",
+      imageUrl: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
+      price: 20,
+    },
+
+    {
+      id: 26,
+      name: "Red Dots Dress",
+      imageUrl: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
+      price: 80,
+    },
+    {
+      id: 27,
+      name: "Striped Sweater",
+      imageUrl: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
+      price: 45,
+    },
+    {
+      id: 28,
+      name: "Yellow Track Suit",
+      imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
+      price: 135,
+    },
+    {
+      id: 29,
+      name: "White Blouse",
+      imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
+      price: 20,
+    },
+  ],
+  jackets: [
+    {
+      id: 18,
+      name: "Black Jean Jacket",
+      imageUrl: "https://i.ibb.co/XzcwL5s/black-shearling.png",
+      price: 125,
+    },
+    {
+      id: 19,
+      name: "Blue Jean Jacket",
+      imageUrl: "https://i.ibb.co/mJS6vz0/blue-jean-jacket.png",
+      price: 90,
+    },
+    {
+      id: 20,
+      name: "Grey Jean Jacket",
+      imageUrl: "https://i.ibb.co/N71k1ML/grey-jean-jacket.png",
+      price: 90,
+    },
+    {
+      id: 21,
+      name: "Brown Shearling",
+      imageUrl: "https://i.ibb.co/s96FpdP/brown-shearling.png",
+      price: 165,
+    },
+    {
+      id: 22,
+      name: "Tan Trench",
+      imageUrl: "https://i.ibb.co/M6hHc3F/brown-trench.png",
+      price: 185,
+    },
+  ],
+  shoes: [
+    {
+      id: 10,
+      name: "White Converse",
+      imageUrl:
+        "https://www.highsnobiety.com/static-assets/thumbor/M9qHqZbWqh6IDj0WuMLrgwNutBE=/1600x1165/www.highsnobiety.com/static-assets/wp-content/uploads/2022/05/01182010/pglang-converse-collab-release-date-price.jpg",
+      price: 110,
+    },
+    {
+      id: 11,
+      name: "Adidas Stan Smith",
+      imageUrl:
+        "https://assets-global.website-files.com/5ff5c4fcf7154c1c91f670a0/625f931e6a48204bc22802cf_adidas-originals-by-parley-ss22-sneaker-collection%20(19).jpg",
+      price: 140,
+    },
+    {
+      id: 12,
+      name: "Black Vans",
+      imageUrl:
+        "https://img01.ztat.net/article/spp-media-p1/c3518d7afa003b199a26956595866440/75521bb5c184407ba00f35542b4c009f.jpg?imwidth=1800",
+      price: 100,
+    },
+    {
+      id: 13,
+      name: "Nike White AirForce",
+      imageUrl: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png",
+      price: 160,
+    },
+    {
+      id: 14,
+      name: "Nike Red High Tops",
+      imageUrl: "https://i.ibb.co/QcvzydB/nikes-red.png",
+      price: 160,
+    },
+    {
+      id: 15,
+      name: "Classic Shoes",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQELxcPwJw1cykvbE-UkMsaMJ834xD20zOZTw&usqp=CAU",
+      price: 250,
+    },
+    {
+      id: 16,
+      name: "B&W Converse",
+      imageUrl:
+        "https://www.converse.com/dw/image/v2/BCZC_PRD/on/demandware.static/-/Sites-cnv-master-catalog/default/dwe3b7f3dc/images/h_08/162050C_H_08X1.jpg?sw=406",
+      price: 190,
+    },
+    {
+      id: 17,
+      name: "Timberlands",
+      imageUrl: "https://i.ibb.co/Mhh6wBg/timberlands.png",
+      price: 200,
+    },
+  ],
+};
+export function getProducts(name) {
+  return products[name];
+}
+export function getProduct(category, id) {
+  const items = products[category];
+  return items.find((product) => product.id === id);
+}
