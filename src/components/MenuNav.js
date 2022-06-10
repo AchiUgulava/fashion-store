75ab3cfc7997c7ce62c415488562c7b2c1fd0a78
Achi <achiugulava@gmail.com>
2022-06-10 18:46:18 +0400

    bug fix

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -7,7 +7,7 @@ const MenuNav= () => {
         <div className="items-center justify-center hidden p-4 space-x-10 font-semibold text-pink-600 md:flex">
         <Link to={"/shop/hats"}>Hats</Link>
         <Link to={"/shop/jackets"}>Jackets</Link>
-        <Link to={"/shop/sneakers"}>Sneakers</Link>
+        <Link to={"/shop/shoes"}>Shoes</Link>
         <Link to={"/shop/womens"}>Womens</Link>
         <Link to={"/shop/mens"}>Mens</Link>
       </div>
